63744b6c19846cb81d476e43553120b1b61dd26b
markocopic <markocopicpp@gmail.com>
2022-02-23 12:15:29 +0100

    Dan_4_23-02-2022

--- src/App.vue
+++ src/App.vue
@@ -1,207 +1,172 @@
 <template>
   <div id="main">
-    <img alt="Vue logo" src="./assets/logo.png">
-     <h1>{{title}}</h1>
-<!-- 
-    <h3>Volume: {{volume}}</h3>
-    <button @click="volume--">Decrease</button>
-    <button @click="volume++">Increase</button> -->
 
+    <div v-if="showModal" class="edit-modal">
+      <input v-model="singlePost.title" type="text"><br>
+      <textarea v-model="singlePost.body" name="" id="" cols="50" rows="10"></textarea><br>
+      <button @click="closeModal()">cancel</button>
+      <button @click="updatePost()">save</button>
 
+    </div>
 
-     <!-- <p @click="showRed = !showRed"
-      class="red-x"
-      :class="{'bg-red':showRed,'nova-klasa':nova}"> Petar Petrovic </p> -->
+    <div class="filters">
+      <select name="" id="" v-model="userId">
+        <option v-for="user in users" :value="user.id" :key="user.id">
+          {{user.name}}
+        </option>
+      </select>
+      
+      <button @click="createPost()">Create Post</button>
+    </div>
 
-<!-- 
-    <span>First name</span><input v-model="firstName" type="text"><br>
-    <span>Last name</span><input v-model="lastName" type="text"><br>
+    <div class="posts-region">
+      <div @click="selectPost(post)" v-for="post in posts" :key="post.id">
+        <h3>{{post.title}}</h3>
+        <p>{{post.body}}</p>
+        <button @click="editPost(post)">edit</button>
+        <button @click="deletePost(post)">delete</button>
+      </div>
+    </div>
 
-    <p>{{ username }}</p> -->
-
-
-      <!-- <button @click="a++">Add to A</button>
-      <button @click="b++">Add to B</button>
-
-     <p>Age + a = {{ addToA }}</p>
-     <p>Age + b = {{ addToB }}</p> -->
-    <!-- <ul>
-      <li v-for="(name,index) in names" :key="index" 
-      @click="name.showRed = !name.showRed"
-       :class="{'bg-red':name.showRed}"> {{name.name}} -->
-         <!-- <span @click="removeName(index)" class="red-x">X</span> -->
-      <!-- </li>
-    </ul>
-    <input v-if="listFull" @keyup.enter="addName()" v-model="inputName" type="text">   -->
-    <!-- <button @click="addName()">Add Name</button> -->
-    <!-- <br>
-    <small style="color:red;" v-if="emptyValue">Empty value !!!</small> -->
-    <!-- <button v-if="show" @click="hideTitle()">Hide title</button>
-    <button v-else @click="show=true">Show title</button> -->
-    <!-- <button @click="title = 'DRUGI NAZIV'">Change Title</button> -->
-    <!-- <button @click="changeNumber('minus')">-</button><br>
-    <h3>{{number}}</h3>
-    <button @click="changeNumber('plus')">+</button> -->
-    <!-- <input v-model="name" type="text"> -->
-    <!-- <button @click="addName()">Add Name</button> -->
-    <!-- <p>{{name}}</p> -->
-    <!-- <input type="radio" value="One" v-model="picked">
-    <label for="One">One</label>
-    <br>
-    <input type="radio" value="Two" v-model="picked">
-    <label for="Two">Two</label>
-    <br>
-    <span>Picked: {{picked}}</span> -->
-    <!-- <button @click="catchEvent($event)">Catch Event</button>
-    <div class="area" @mousemove="getCoo($event)" @mouseleave="resetCoo()">
-      <p>{{x}}, {{y}}</p>
-    </div> -->
-<!-- <br>
-    <input type="checkbox" name="Milos" id="milos" value="Milos" v-model="names">
-    <label for="milos">Milos</label>
-    <br>
-    <input type="checkbox" name="Zoran" id="zoran" value="Zoran" v-model="names">
-    <label for="zoran">Zoran</label>
-    <br>
-    <input type="checkbox" name="Goran" id="goran" value="Goran" v-model="names">
-    <label for="goran">Goran</label>
-    <br>
-    <span>Names: {{names}}</span> -->
-
-  <!-- <select v-model="selected">
-    <option v-for="option in options" 
-    :value="option"
-    :key="option.value">
-      {{option.text}}
-    </option>
-  </select>
-  <p>{{ selected }}</p> -->
-
-  <button @click="getData()">Get data from service</button>
-
-  <ul>
-    <li v-for="user in users" :key="user.id">
-      {{user.name}} , {{user.address.street}} - {{user.address.suite}}
-
-    </li>
-  </ul>
-
-
+    <div class="single-post">
+     <h2>Post info:</h2>
+     <hr>
+     <h3>{{singlePost.title}}</h3>
+     <p>{{singlePost.body}}</p>
+     <hr>
+     <h4>Comments:</h4>
+     <ul>
+       <li v-for="comment in comments" :key="comment.id"> 
+        <p> {{comment.name}}</p>
+        <span>{{comment.body}}</span><br>
+        <small>email: {{comment.email}}</small>
+        <hr>
+      </li>
+     </ul>
+    </div>
 
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+ import axios from 'axios'
 
 export default {
   data(){
     return{
-      volume:0,
-      showRed:true,
-      age:20,
-      a:0,
-      b:0,
       title: 'VUE OBUKA',
-      number: 0,
-      name:'Mirko',
-      inputValue:'',
-      picked:'Two',
-      x:0,
-      y:0,
-      // names:[
-      //   {name:'Goran', showRed:false},
-      //   {name:'Zoran', showRed:false},
-      //   {name:'Petar', showRed:false},
-      //   {name:'Jovan', showRed:false}
-      //   ],
-      inputName:'',
-      emptyValue:false,
-      selected:'',
-      options:[
-        {value:1, text:'Jedan'},
-        {value:2, text:'Dva'},
-        {value:3, text:'Tri'}
-      ],
-      firstName:'',
-      lastName:'',
-      users:[]
+      userId:1,
+      posts:[],
+      users:[],
+      comments:[],
+      singlePost:{},
+      showModal:false,
+      newPost:false
     }
   },
+  mounted(){
+    this.getData()
+    this.getUsers()
+  },
   watch:{
-    volume(newVal,oldVal){
-      if (newVal == 8 && oldVal<newVal) {
-        alert('Too loud !!!')
-      }
+    userId(){
+      this.getData()
     }
   },
   computed:{
-    listFull(){
-      if (this.names.lenght > 4) {
-        return false
-      } else {
-        return true
-      }
-    },
-    username(){
-      if (this.firstName && this.lastName) {
-      return this.firstName.toUpperCase() +'.'+ this.lastName.toUpperCase()
-      } else {
-        return ''
-      }
-    },
-    addToA(){
-      console.log('Calculate A');
-      return this.age + this.a
-    },
-    addToB(){
-      console.log('Calculate B');
-      return this.age + this.b
-    }
   },
   methods:{
-    getData(){
-      axios.get('https://jsonplaceholder.typicode.com/users').then(
-        response=>{
-          console.log(response.data);
-          this.users = response.data
+    createPost(){
+      this.showModal = true
+      this.newPost = true
+    },
+    updatePost(){
+      console.log(this.selectPost);
+      if (this.newPost) {
+        axios.post('https://jsonplaceholder.typicode.com/posts',{
+          title: this.singlePost.title,
+          body: this.singlePost.body,
+          userId: this.userId,
+        }).then(res=>{
+          console.log(res.data);
+          this.newPost = false
+          this.showModal = false
+          this.singlePost = {}
+          alert('Post created')
+        })
+      } else {
+        axios.put('https://jsonplaceholder.typicode.com/posts/'+this.singlePost.id,this.singlePost)
+        .then(res=>{
+          console.log(res.data)
+          this.showModal = false
+          this.singlePost = {}
+          alert('Post updated')
+        })
+      }
+      
+    },
+    closeModal(){
+      this.showModal = false
+    },
+    deletePost(post){
+      axios.delete('https://jsonplaceholder.typicode.com/posts/'+post.id)
+      .then(res=>{
+        console.log(res.data);
+      })
+    },
+    editPost(){
+      this.showModal = true
+    },
+    getComments(){
+      axios.get('https://jsonplaceholder.typicode.com/comments',{
+        params:{
+          postId: this.singlePost.id
+        }
+      }).then(
+        res=>{
+          this.comments = res.data
+          console.log(this.comments);
         }
       )
     },
-    removeName(i){
-      this.names.splice(i,1)
+    selectPost(post){
+      this.singlePost = post
+      this.getComments()
     },
-    addName(){
-      if (this.inputName) {
-        this.names.push(this.inputName)
-        this.emptyValue = false
-      }else{
-        this.emptyValue = true
-      }
-      this.inputName = ''
-    },
-    hideTitle(){
-      this.show = false
-    },
-    resetCoo(){
-      this.x = 0
-      this.y = 0
-    },
-    getCoo(e){
-      this.x = e.offsetX
-      this.y = e.offsetY
-    },
-    catchEvent(e){
-      console.log('EVENT OBJEKAT',e);
-    },
-    changeNumber(operation){
-      if (operation == 'plus') {
-        this.number = this.number + 1
-      } else if(operation == 'minus') {
-        this.number = this.number - 1
-      }
-      console.log(this.number)
+    getUsers(){
+      axios.get('https://jsonplaceholder.typicode.com/users',{
+        params:{
+        }
+      }).then(
+        res=>{
+          this.users = res.data
+          console.log(this.users);
 
+        }
+      )
+    },
+    getData(){
+      this.posts = []
+      axios.get('https://jsonplaceholder.typicode.com/posts',{
+        params:{
+          userId:this.userId
+        }
+      }).then(
+        res=>{
+          this.posts = res.data
+          console.log(res);
+
+        }
+      )
+      // .catch(e=>{
+      //   if (e.response.status == 404) {
+      //     console.log(e.response.status);
+      //   } else {
+      //     console.log(e);
+          
+      //   }
+      //   alert('Serevr error')
+      // })
     }
   }
 }
@@ -216,16 +181,30 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.area{
-  background-color:light-grey;
-  min-height: 500px;
+.post{
   border: 1px solid grey;
-}
-.red-x{
+  margin: 20px;
   cursor: pointer;
-  color:red;
 }
-.bg-red{
-  background-color: red;;
+.posts-region{
+  max-height: 40vh;
+  overflow: auto;
+  border: 1px solid grey;
+  margin-top: 10px;
+}
+.single-post{
+  max-height: 40vh;
+  border: 1px solid grey;
+  margin-top:20px;
+  overflow: auto;
+
+}
+.edit-modal{
+  position: absolute;
+  top: 30%;
+  left:30%;
+  z-index: 1000;
+  background-color: lightgrey;
+  padding: 20px;
 }
 </style>
